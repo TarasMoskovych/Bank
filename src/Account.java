9cfe395b846f7e73256c4137d1bba8ac6029e951
Taras_Moskovych <moskovych.taras@gmail.com>
2017-06-05 14:51:33 +0300

    added fields, constructors, getters and setters and method toString to classes Account and Transaction

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,99 @@
 
 public class Account {
 
+	private int id;
+	private String name;
+	private String surName;
+	private String login;
+	private String password;
+	private float amount;
+	
+	public Account(int id, String name, String surName, String login, String password, float amount) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.surName = surName;
+		this.login = login;
+		this.password = password;
+		this.amount = amount;
+	}
+		
+	public Account() {
+		super();
+	}
+
+	public Account(String name, String surName, String login, String password, float amount) {
+		super();
+		this.name = name;
+		this.surName = surName;
+		this.login = login;
+		this.password = password;
+		this.amount = amount;
+	}
+	
+	public Account(String name, String surName) {
+		super();
+		this.name = name;
+		this.surName = surName;
+	}
+	
+	public Account(String name) {
+		super();
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurName() {
+		return surName;
+	}
+
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public float getAmount() {
+		return amount;
+	}
+
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", name=" + name + ", surName=" + surName + ", login=" + login + ", password="
+				+ password + ", amount=" + amount + "]";
+	}
+	
 }
--- src/Transaction.java
+++ src/Transaction.java
@@ -1,4 +1,93 @@
 
 public class Transaction {
 
+	private int id;
+	private String name;
+	private String type;
+	private String date;
+	private float amount;
+	private int id_Account;
+	
+	public Transaction(int id, String name, String type, String date, float amount, int id_Account) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.type = type;
+		this.date = date;
+		this.amount = amount;
+		this.id_Account = id_Account;
+	}
+	
+	public Transaction(String name, String type, String date, float amount) {
+		super();
+		this.name = name;
+		this.type = type;
+		this.date = date;
+		this.amount = amount;
+			}
+	
+	public Transaction(String name, String type, String date, float amount, int id_Account) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.type = type;
+		this.date = date;
+		this.amount = amount;
+		this.id_Account = id_Account;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public float getAmount() {
+		return amount;
+	}
+
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+
+	public int getId_Account() {
+		return id_Account;
+	}
+
+	public void setId_Account(int id_Account) {
+		this.id_Account = id_Account;
+	}
+
+	@Override
+	public String toString() {
+		return "Transaction [id=" + id + ", name=" + name + ", type=" + type + ", date=" + date + ", amount=" + amount
+				+ ", id_Account=" + id_Account + "]";
+	}
+
 }
