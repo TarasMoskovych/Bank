ec3f097277a42ec1d1acee0a895df8518e49dad0
Taras_Moskovych <moskovych.taras@gmail.com>
2017-06-05 15:23:24 +0300

    created new database in system
    1. added mysql-connector driver;
    2. configured connection.

--- src/Operations.java
+++ src/Operations.java
@@ -1,4 +1,11 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Operations {
+	
+	public static void start() throws SQLException{
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/bank_db?createDatabaseIfNotExist=true", "root", "123456");
+	}
 
 }
